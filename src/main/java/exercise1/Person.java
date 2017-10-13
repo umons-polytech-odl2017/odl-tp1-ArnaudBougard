6cfd745fc9f80583087cce8625dde73c90e7bd81
151828 <ab_7110@live.fr>
2017-10-13 14:52:09 +0200

    Variables, constructeur et getters écrits.

--- src/main/java/exercise1/Person.java
+++ src/main/java/exercise1/Person.java
@@ -1,4 +1,19 @@
 package exercise1;
 
 class Person {
+
+	private String name;
+	private int age;
+
+	public Person(){
+
+		name = "default value";
+		age = 0;
+
+	}
+
+	public String getName() { return name;}
+
+	public int getAge(){ return age;}
+
 }
