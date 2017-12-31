b1c7697e61c5bde8b6441d652887feed4c8f05c9
ArnaudBougard <ab_7110@live.fr>
2017-12-31 12:42:43 +0100

    exercice2/Person.java terminé

--- src/main/java/exercise2/Person.java
+++ src/main/java/exercise2/Person.java
@@ -1,4 +1,41 @@
 package exercise2;
 
 class Person {
+	
+	private int age;
+    private static int populationSize=0, totalAge=0;
+    private static float averagePopulationAge=0.0F;
+
+    // ASTUCE sur IntelliJ : Après avoir défini les variables membres, utiliser le raccourci ALT+INSERT pour générer automatiquement constructeur, getters, setters ou toStrings !
+
+    public Person(int age) {
+
+        this.age = age;
+        populationSize++;
+        totalAge=totalAge+age;
+
+    }
+
+    static int computePopulationSize(){
+
+        return populationSize;
+
+    }
+
+    static float computeAveragePopulationAge(){
+
+        averagePopulationAge=(float)totalAge/populationSize; // Au moins une des deux variables "totalAge" ou "populationSize" doit être précédée de "(float)" sinon la réponse est arrondie !
+
+        return averagePopulationAge;
+
+    }
+
+    static void resetPopulation(){
+
+        populationSize=0;
+        totalAge=0;
+        averagePopulationAge=0;
+
+    }
+	
 }
